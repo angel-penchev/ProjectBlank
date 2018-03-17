214aec3d380c66ecb71832c1e467abb875f8f8d5
bobig6 <b.i.ivanov1@gmail.com>
2018-03-17 11:32:50 +0200

    Adding Numpad System
    The only thing done is Numpad System

---
+++ Assets/Objects/Props/Numpad/Button0.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button0 : MonoBehaviour
+{
+
+
+	public buttonPress bp;
+	public int but0 = 0;
+	
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnMouseDown()
+	{
+		bp.codeNow.Add(but0);
+
+	}
+}
